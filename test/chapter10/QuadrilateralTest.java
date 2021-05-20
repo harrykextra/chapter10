6aebc06f93af0a9cfdf06af769f5686506fb9cb9
harrykextra <harrykextra@gmail.com>
2021-05-20 17:05:09 +0100

    added a new test method

--- test/chapter10/QuadrilateralTest.java
+++ test/chapter10/QuadrilateralTest.java
@@ -2,6 +2,7 @@ package chapter10;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +11,15 @@ class QuadrilateralTest {
 	@BeforeEach
 	void setUp() throws Exception {
 	}
+	@AfterEach
+	void deleteAfterUse() {
+		
+	}
 
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
+	
 
 }

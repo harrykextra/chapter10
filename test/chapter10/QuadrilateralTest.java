8e9e1c242c9508e10b6aded12ad4a6012be4fc84
harrykextra <harrykextra@gmail.com>
2021-05-20 16:53:05 +0100

    intial commit

---
+++ test/chapter10/QuadrilateralTest.java
@@ -0,0 +1,19 @@
+package chapter10;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class QuadrilateralTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/payments/PayableTest.java
@@ -0,0 +1,14 @@
+package payments;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PayableTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
